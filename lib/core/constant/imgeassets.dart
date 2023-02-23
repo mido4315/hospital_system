15594bc90dedd04b0eb78aa2e7638837f0458d86
michael3620 <nonanady362@gmail.com>
2023-02-23 20:59:14 +0200

    tasks

---
+++ lib/core/constant/imgeassets.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: constant_identifier_names
+
+class ImageAssets{
+ static const String imageRoot = 'Assets';
+ static const String newOrder = '$imageRoot/neworderappbaricon.png';
+ static const String edit = '$imageRoot/edit.png';
+ static const String delete = '$imageRoot/delete.png';
+ static const String homeIndicator = '$imageRoot/homeindicator.png';
+ static const String cancel = '$imageRoot/cancel.png';
+ static const String phoneCancel = '$imageRoot/pngphone-call.png';
+ static const String prophile = '$imageRoot/2.1.png';
+ static const String splash = '$imageRoot/splash.png';
+ static const String intro1 = '$imageRoot/intro1.png';
+ static const String intro2 = '$imageRoot/intro2.png';
+ static const String intro3 = '$imageRoot/intro3.png';
+ static const String BackgroundIntro = '$imageRoot/background.png';
+
+
+}
