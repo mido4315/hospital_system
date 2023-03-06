32d1ab431c37a8b2168973e094bef54da3976b73
mohamedanwarr <mohamedanwarr@gmial.com>
2023-03-06 16:55:39 +0200

    attendance and leaving screens widgets

--- lib/features/Doctor , Case/Presention/Viwes/Screens/CaseDetails.dart
+++ lib/features/Doctor , Case/Presention/Viwes/Screens/CaseDetails.dart
@@ -133,24 +133,3 @@ class _CaseDetailsState extends State<CaseDetails> {
   }
 }
 
-class TextWidgets extends StatelessWidget {
-   TextWidgets({
-    required this.title,
-    required this.subtitle,
-  });
-    String title;
-    String subtitle;
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Text(title,style: TextStyle(color: Colors.grey)),
-          Text(subtitle),
-        ],
-      ),
-    );
-  }
-}
